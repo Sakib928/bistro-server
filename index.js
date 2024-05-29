b593d483e8cb6664ec28fb085a9d26eaa0b9c562
Sakib928 <sakibgscian@gmail.com>
2024-05-29 15:46:59 +0600

    created user request

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1towayy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,6 +29,30 @@ async function run() {
         const menuCollection = client.db('bistroDB').collection('menu');
         const reviewCollection = client.db('bistroDB').collection('reviews');
         const cartCollection = client.db('bistroDB').collection('carts');
+        const userCollection = client.db('bistroDB').collection('users');
+
+        // user related api
+        app.get('/users', async (req, res) => {
+            const result = await userCollection.find().toArray();
+            res.send(result);
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const userExists = await userCollection.findOne(query);
+            if (userExists) {
+                return res.send({ message: 'user already exists', insertedId: null })
+            }
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
+        // menu related api
         app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
             res.send(result);
@@ -38,10 +62,24 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/carts', async (req, res) => {
+            const userEmail = req.query.email;
+            // console.log(userEmail);
+            const query = { email: userEmail };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.post('/carts', async (req, res) => {
             const item = req.body;
             // console.log(req.body);
             const result = await cartCollection.insertOne(item);
+            res.send(result);
+        })
+        app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await cartCollection.deleteOne(filter);
             res.send(result);
         })
         // Send a ping to confirm a successful connection
